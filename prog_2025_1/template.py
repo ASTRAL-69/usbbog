0d947ca81c05f15589c13b3ca02afc1ea346c8e3
ASTRAL-69 <angeluniversal69@gmail.com>
2025-03-02 13:40:53 -0500

    Update template.py
    Prototipo de calculadora 1

--- prog_2025_1/template.py
+++ prog_2025_1/template.py
@@ -1,29 +1,51 @@
 #!/usr/bin/env python3
 
 """
-Título de práctica: breve descripción
+#ejercicio 1= calculadora basica
 
-Descripción extendida del programa
+este es un ejemplo de como usar las instrucciones basicas para usar python
 
-Autor: ElAutor <el@correo>
-Fecha: 2025-02-01
+#autor: Angel Rubio
+#correo: angeluniversal69@gmail.com
+#fecha: 20/02/25
 """
-
-# **** En esta región puede importar los módulos necesarios para su programa
-# **** O las definiciones de clases y/o funciones que requiera.
-
 
 def run():
     """script entrypoint"""
+    
+#recopilar operandos
+primerTermino = int(input("escriba un  numero entero: "))
+segundoTermino = int(input("escriba otro  numero entero: "))
 
-    # **** Poner el código ejecutable de su ejercicio aquí
+#hacer la operacion suma
+resultadoSuma = primerTermino + segundoTermino
 
-    # Saludo
-    print("Hola mundo!")
+#hacer la operacion resta
+resultadoResta = primerTermino - segundoTermino
 
-    # **** ****
+#hacer la operacion multiplicacion
+resultadoMultiplicacion = primerTermino * segundoTermino
 
+#hacer la operacion division
+resultadoDivision = primerTermino / segundoTermino
 
-# **** Conserve este condicional para ejecutar el programa directamente
+#hacer la operacion potenciacion
+resultadoPotenciacionPrimerTermino = primerTermino **2
+
+#hacer la operacion potenciacion
+resultadoPotenciacionSegundoTermino = segundoTermino **2
+#mostrar numero en pantalla
+print (f"la suma de {primerTermino} y {segundoTermino} es {resultadoSuma}")
+
+print (f"la resta de {primerTermino} y {segundoTermino} es {resultadoResta}")
+
+print (f"la multiplicacion de {primerTermino} y {segundoTermino} es {resultadoMultiplicacion}")
+
+print (f"la division de {primerTermino} y {segundoTermino} es {resultadoDivision}")
+
+print (f"el cuadrado del primer termino {primerTermino}  {resultadoPotenciacionPrimerTermino}")
+
+print (f"el cuadrado del segundo termino  {segundoTermino} es {resultadoPotenciacionSegundoTermino}")
+
 if __name__ == "__main__":
     run()
